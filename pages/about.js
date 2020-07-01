5cb3ef6a1a1d12b9dd631f59e5c3a06314da25fb
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-07-02 00:05:56 +0200

    Adding nav bar and project config

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import Nav from '../components/Nav';
 
 const About = () => (
     <div>
+    	<Nav />
         <h2>ABOUT PAGE</h2>
 	    <Link href="/">
 	        <a>Go to index page</a>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Nav from '../components/Nav';
 
 const Index = () => (
     <div>
+        <Head>
+            <title>Home page</title>
+            <meta name="description" content="SSR React with Next JS" />
+            <meta name="keywords" content="react next ssr" />
+            <meta name="author" content="Adam Carroll" />
+        </Head>
+    	<Nav />
         <h2>HOME PAGE</h2>
         <Link href="/about">
             <a>Go to about page</a>
