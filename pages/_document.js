baa0b28b02b89e801c8f1c9d3b25afc2a5d25205
Adam Carroll <Oceloteater@users.noreply.github.com>
2020-07-02 21:21:03 +0200

    Submitting search query example reload results

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
                     <meta name="description" content="SSR React with Next JS" />
                     <meta name="keywords" content="react next ssr" />
                     <meta name="author" content="Adam Carroll" />
-                    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous" />
+                    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossOrigin="anonymous" />
                 </Head>
                 <body>
                     <Main />
--- pages/news.js
+++ pages/news.js
@@ -1,13 +1,35 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import Router from 'next/router';
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-fetch';
 
 const News = ({ news }) => {
+    const [query, setQuery] = useState('react');
+
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        Router.push(`/news?searchTerm=${query}`);
+    };
+
+    const searchArticles = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={query} onChange={handleChange}></input>
+            <button>Search</button>
+        </form>
+    );
+
     return (
         <Layout title="News">
             <div>
                 <h2>List of headlines</h2>
+                <hr/>
+                { searchArticles() }
                 <hr/>
                 { news.hits.map((n, i) => (
                     <p key={i}>
@@ -22,11 +44,11 @@ const News = ({ news }) => {
 //{ JSON.stringify(news) }
 
 
-News.getInitialProps = async () => {
+News.getInitialProps = async ({ query }) => {
     let news;
     try {
         //const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-        const res = await fetch('https://hn.algolia.com/api/v1/search?query=react');
+        const res = await fetch(`https://hn.algolia.com/api/v1/search?query=${query.searchTerm}`);
         news = await res.json();
         console.log(news);
     } catch (error) {
